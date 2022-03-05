cf8e764ce1cdedd1d0ad3f1bede8216693f77fbe
ManczakAdrian <manczakadrian@gmail.com>
2022-03-06 00:55:38 +0100

    Initial commit

---
+++ src/main/java/pl/manczak/ksb2videoapp/Ksb2VideoAppApplication.java
@@ -0,0 +1,13 @@
+package pl.manczak.ksb2videoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksb2VideoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ksb2VideoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/manczak/ksb2videoapp/Ksb2VideoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.manczak.ksb2videoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksb2VideoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
